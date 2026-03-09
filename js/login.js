76db8810bef53fe0fcdabafad9e3b5534d364e84
Khurshid Alam <khurshidalam741@gmail.com>
2026-03-09 09:52:26 +0600

    login UI design complate and function complete

---
+++ js/login.js
@@ -0,0 +1,33 @@
+const PressLogin = () => {
+
+    const Username = document.getElementById("user_name").value;
+    const Password = document.getElementById("password").value;
+
+    if (Username.trim() == null || Username.trim() == "") {
+        alert("Please enter your username ")
+        return;
+    }
+
+    if (Password.trim() == null || Password.trim() == "") {
+        alert("Please enter your password ")
+        return;
+    }
+
+    const CorrectUser = "admin";
+    const CorrectPassword = "admin123";
+
+    if (Username.trim() === CorrectUser && Password.trim() === CorrectPassword) {
+
+       alert("successfully login");
+        window.location.assign("home.html");
+
+    } else {
+        alert("Credentials Not Match!! Please Try Again");
+        return;
+    }
+
+
+
+    console.log(Username, Password);
+
+}
---
+++ js/menu_toggle.js
@@ -0,0 +1,16 @@
+
+let MenuOpen = false;
+const ToggleMenu = ()=>{
+
+    console.log("click");
+     const menu = document.getElementById("small_device");
+
+     if(MenuOpen==false){
+        menu.classList.remove("hidden");
+        MenuOpen=true;
+     }else if(MenuOpen==true){
+        menu.classList.add("hidden");
+        MenuOpen=false;
+     }
+
+}
